e748ab59a4520721986c4ceabe37a660bec1d610
sihabs-dev <mdsarks123@gmail.com>
2026-02-24 05:56:45 +0600

    add interview and reject toggle

--- script.js
+++ script.js
@@ -14,9 +14,12 @@ let interviewList = [];
 let rejectedList = [];
 
 function calculateCard() {
-  totalCount.innerText = allCards.children.length;
   interviewCount.innerText = interviewList.length;
   rejectedCount.innerText = rejectedList.length;
+  totalCount.innerText =
+    allCards.children.length -
+    interviewCount.innerText -
+    rejectedCount.innerText;
 }
 calculateCard();
 
@@ -51,9 +54,141 @@ mainSection.addEventListener("click", function (event) {
       cardHeading,
       cardInfo,
       cardSalary,
-      cardStatus,
+      cardStatus: "interview",
       cardPara,
-      };
-      console.log(cardInfomation)
+    };
+    // console.log(cardInfomation);
+    let isExit = interviewList.find(
+      (item) => item.cardHeading == cardInfomation.cardHeading,
+    );
+    if (!isExit) {
+      interviewList.push(cardInfomation);
+    }
+
+    renderInterviewCard();
+    calculateCard();
+  } else if (event.target.classList.contains("rejected-btn")) {
+    let cardDiv = event.target.closest(".card");
+    let cardHeading = cardDiv.querySelector(".heading").textContent;
+    let cardInfo = cardDiv.querySelector(".info").textContent;
+    let cardSalary = cardDiv.querySelector(".salary").textContent;
+    let cardStatus = cardDiv.querySelector(".status").textContent;
+    let cardPara = cardDiv.querySelector(".para").textContent;
+
+    let cardInfomation = {
+      cardHeading,
+      cardInfo,
+      cardSalary,
+      cardStatus: "rejected",
+      cardPara,
+    };
+    // console.log(cardInfomation);
+    let isExit = rejectedList.find(
+      (item) => item.cardHeading == cardInfomation.cardHeading,
+    );
+    if (!isExit) {
+      rejectedList.push(cardInfomation);
+    }
+
+    renderRejectedCard();
+    calculateCard();
   }
 });
+
+function renderInterviewCard() {
+  getFilterSection.innerHTML = "";
+
+  for (const makeCard of interviewList) {
+    let createNewDiv = document.createElement("div");
+    createNewDiv.className = "card space-y-3 p-5 shadow-sm rounded-md relative";
+    createNewDiv.innerHTML = `
+    <div>
+              <h2 class="heading text-2xl font-bold">${makeCard.cardHeading}</h2>
+              <p class="info text-gray-500">${makeCard.cardInfo}</p>
+            </div>
+            <div>
+              <p class="salary text-gray-600">
+                ${makeCard.cardSalary}
+              </p>
+            </div>
+            <div class="px-3.5 py-2 bg-black/15 inline-block rounded-md">
+              <p class="status">${makeCard.cardStatus}</p>
+            </div>
+            <div>
+              <p class="para text-gray-700">
+                ${makeCard.cardPara}
+              </p>
+            </div>
+            <!-- interview and rejected btn -->
+            <div>
+              <button
+                
+                class="interview-btn text-green-500 px-3 py-1.5 border-2 border-green-500 rounded-sm"
+              >
+                INTERVIEW
+              </button>
+              <button
+               
+                class="rejected-btn text-red-500 px-3 py-1.5 border-2 border-red-500 rounded-sm"
+              >
+                REJECTED
+              </button>
+            </div>
+            <div class="absolute top-7 right-7">
+              <button id="delete-btn">
+                <img src="./images/delete.png" alt="" />
+              </button>
+            </div>
+    `;
+    getFilterSection.appendChild(createNewDiv);
+  }
+}
+
+function renderRejectedCard() {
+  getFilterSection.innerHTML = "";
+
+  for (const makeCard of rejectedList) {
+    let createNewDiv = document.createElement("div");
+    createNewDiv.className = "card space-y-3 p-5 shadow-sm rounded-md relative";
+    createNewDiv.innerHTML = `
+    <div>
+              <h2 class="heading text-2xl font-bold">${makeCard.cardHeading}</h2>
+              <p class="info text-gray-500">${makeCard.cardInfo}</p>
+            </div>
+            <div>
+              <p class="salary text-gray-600">
+                ${makeCard.cardSalary}
+              </p>
+            </div>
+            <div class="px-3.5 py-2 bg-black/15 inline-block rounded-md">
+              <p class="status">${makeCard.cardStatus}</p>
+            </div>
+            <div>
+              <p class="para text-gray-700">
+                ${makeCard.cardPara}
+              </p>
+            </div>
+            <!-- interview and rejected btn -->
+            <div>
+              <button
+                
+                class="interview-btn text-green-500 px-3 py-1.5 border-2 border-green-500 rounded-sm"
+              >
+                INTERVIEW
+              </button>
+              <button
+               
+                class="rejected-btn text-red-500 px-3 py-1.5 border-2 border-red-500 rounded-sm"
+              >
+                REJECTED
+              </button>
+            </div>
+            <div class="absolute top-7 right-7">
+              <button id="delete-btn">
+                <img src="./images/delete.png" alt="" />
+              </button>
+            </div>
+    `;
+    getFilterSection.appendChild(createNewDiv);
+  }
+}
